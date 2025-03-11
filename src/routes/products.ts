070b07b72c92f80a67edf4b93f23282688796f6b
Mick Negenman <36791@ma-web.nl>
2025-03-11 15:22:41 +0100

    Added put

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -6,8 +6,8 @@ import { zValidator } from "@hono/zod-validator";
 // Schemas
 const productsQuerySchema = z.object({
     filter: z.string().optional(),
-    minPrice: z.coerce.number().optional(),
-    maxPrice: z.coerce.number().optional(),
+    minPrice: z.coerce.number().nonnegative().optional(),
+    maxPrice: z.coerce.number().nonnegative().optional(),
     order: z.enum(["asc", "desc"]).optional(),
     limit: z.coerce.number().int().nonnegative().optional(),
 });
@@ -16,7 +16,7 @@ const newProductSchema = z.object({
     title: z.string().max(50),
     description: z.string().max(500).optional(),
     image: z.string().url().max(255).optional(),
-    price: z.number(),
+    price: z.number().nonnegative(),
 });
 
 const productIdSchema = z.object({
@@ -34,23 +34,38 @@ productsRoute.get("/", zValidator("query", productsQuerySchema), async (c) => {
         ${filter !== undefined ? sql`AND "title" ILIKE ${`%${filter}%`}` : sql``}
         ${minPrice !== undefined ? sql`AND "price" >= ${minPrice}` : sql``}
         ${maxPrice !== undefined ? sql`AND "price" <= ${maxPrice}` : sql``}
-        ${order !== undefined ? sql`ORDER BY "date_added" ${order === "asc" ? sql`ASC` : sql`DESC`}` : sql``}
+        ${order !== undefined ? sql`ORDER BY "date_added" ${sql.unsafe(order)}` : sql``}
         ${limit !== undefined ? sql`LIMIT ${limit}` : sql``}
     `);
 });
 
 productsRoute.post("/", zValidator("json", newProductSchema), async (c) => {
-    await sql`INSERT INTO "products" ${sql(c.req.valid("json"))}`;
+    const data = c.req.valid("json");
+    await sql`INSERT INTO "products" ${sql(data)}`;
     return c.body(null, 201);
 });
 
 productsRoute.get("/:id", zValidator("param", productIdSchema), async (c) => {
-    return c.json((await sql`SELECT * FROM "products" WHERE "id" = ${c.req.valid("param").id}`)[0]);
+    const { id } = c.req.valid("param");
+    return c.json(await sql`SELECT * FROM "products" WHERE "id" = ${id}`);
 });
 
 productsRoute.delete("/:id", zValidator("param", productIdSchema), async (c) => {
-    await sql`DELETE FROM "products" WHERE "id" = ${c.req.valid("param").id}`;
+    const { id } = c.req.valid("param");
+    await sql`DELETE FROM "products" WHERE "id" = ${id}`;
     return c.body(null, 204);
-})
+});
+
+productsRoute.put("/:id",
+    zValidator("param", productIdSchema),
+    zValidator("json", newProductSchema.partial()),
+    async (c) => {
+        const { id } = c.req.valid("param");
+        const data = c.req.valid("json");
+
+        await sql`UPDATE "products" SET ${sql(data)} WHERE "id" = ${id}`;
+        return c.body(null, 204);
+    }
+);
 
 export default productsRoute;
