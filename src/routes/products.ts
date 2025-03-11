8c084510e854d6babba10d47bb55ca024b90fff7
Mick Negenman <36791@ma-web.nl>
2025-03-11 14:35:35 +0100

    added delete

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -4,29 +4,29 @@ import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 
 // Schemas
-const getProductsSchema = z.object({
+const productsQuerySchema = z.object({
     filter: z.string().optional(),
     minPrice: z.coerce.number().optional(),
     maxPrice: z.coerce.number().optional(),
     order: z.enum(["asc", "desc"]).optional(),
-    limit: z.coerce.number().optional(),
+    limit: z.coerce.number().int().nonnegative().optional(),
 });
 
-const createProductSchema = z.object({
+const newProductSchema = z.object({
     title: z.string().max(50),
     description: z.string().max(500).optional(),
     image: z.string().url().max(255).optional(),
     price: z.number(),
 });
 
-const getProductSchema = z.object({
-    id: z.coerce.number().int().min(0),
+const productIdSchema = z.object({
+    id: z.coerce.number().int().nonnegative(),
 });
 
 // Routing
 const productsRoute = new Hono();
 
-productsRoute.get("/", zValidator("query", getProductsSchema), async (c) => {
+productsRoute.get("/", zValidator("query", productsQuerySchema), async (c) => {
     const { filter, minPrice, maxPrice, order, limit } = c.req.valid("query");
 
     return c.json(await sql`
@@ -39,12 +39,18 @@ productsRoute.get("/", zValidator("query", getProductsSchema), async (c) => {
     `);
 });
 
-productsRoute.post("/", zValidator("json", createProductSchema), async (c) => {
+productsRoute.post("/", zValidator("json", newProductSchema), async (c) => {
     await sql`INSERT INTO "products" ${sql(c.req.valid("json"))}`;
+    return c.body(null, 201);
 });
 
-productsRoute.get("/:id", zValidator("param", getProductSchema), async (c) => {
+productsRoute.get("/:id", zValidator("param", productIdSchema), async (c) => {
     return c.json((await sql`SELECT * FROM "products" WHERE "id" = ${c.req.valid("param").id}`)[0]);
 });
+
+productsRoute.delete("/:id", zValidator("param", productIdSchema), async (c) => {
+    await sql`DELETE FROM "products" WHERE "id" = ${c.req.valid("param").id}`;
+    return c.body(null, 204);
+})
 
 export default productsRoute;
